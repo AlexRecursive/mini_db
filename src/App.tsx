9c59bd74d14629ba9daf20fe354068795749b76b
Alex <reload.acc@gmail.com>
2022-10-04 11:29:40 +0300

    Widget CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import 'styles/common.scss';
-import { ReactComponent as Close } from 'images/close.svg';
-import _ from 'lodash';
-import { IItems, itemsDefault } from 'data';
+// import { ReactComponent as Close } from 'images/close.svg';
+import { IItems, itemsDefault, operators } from 'data';
+import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function App() {
   const ResponsiveGridLayout = WidthProvider(Responsive);
@@ -13,11 +15,11 @@ function App() {
   const [items, setItems] = useState<IItems[]>(itemsDefault);
   const [newCounter, setNewCounter] = useState<number>(1);
 
-  const onDelete = (i: number) => {   
-    setItems(_.reject(items, (item, index) => { 
-      return i === index 
-    }))
-  }
+  // const onDelete = (i: number) => {   
+  //   setItems(_.reject(items, (item, index) => { 
+  //     return i === index 
+  //   }))
+  // }
 
   useEffect(()=>{
     const dashboard = localStorage.getItem('dashboard');
@@ -42,7 +44,8 @@ function App() {
         x: 0, 
         y: 0, 
         w: 1, 
-        h: 1
+        h: 1,
+        minW: 3
       }
     ]))
     setNewCounter(newCounter + 1);
@@ -57,9 +60,11 @@ function App() {
       <h1>📊 Mini Dashboard</h1>
       <ResponsiveGridLayout
         className="layout"
-        breakpoints={{ lg: 800, md: 500, sm: 400, xs: 300, xxs: 0 }}
-        rowHeight={120}
-        cols={{ lg: 10, md: 6, sm: 4, xs: 2, xxs: 2 }}
+        rowHeight={83}
+        breakpoints={{ sm: 700, xs: 560, xxs: 0 }}
+        cols={{ sm: 8, xs: 6, xxs: 3 }}
+        resizeHandles={['s','n','w','e']}
+        margin={[6,6]}
         useCSSTransforms={false}
         onDragStop={onChange}
         onResizeStop={onChange}
@@ -71,8 +76,56 @@ function App() {
                 <div key={item.i} onClick={()=>onAdd()} className='react-grid-item-add' data-grid={{...item}}><span>+</span></div>
               ) : (
                 <div key={item.i} data-grid={{...item}}>
-                  {item.i}
-                  <span className='react-grid-item-delete' onClick={()=>onDelete(key)}><Close /></span>
+                  <div className='react-grid-item_main'>
+                    <div className='react-grid-item_top'>
+                      <div className='react-grid-item_title'>
+                        Ваша смена
+                      </div>
+                      {item.h === 1 && (
+                        <div className='react-grid-item_top-info'>
+                          Операторов на смене:
+                          <div className='react-grid-item_amount'>4</div>
+                        </div>
+                      )}
+                    </div>
+                    <div className='react-grid-item_pane'>
+                      <div className='react-grid-item_timer'>
+                        00:19:54
+                      </div>
+                      <div className='react-grid-item_timer-action'>
+                        <div className='react-grid-item_btn'>
+                          Пауза
+                          <PauseCircleFilledIcon />
+                        </div>
+                        <div className='react-grid-item_btn'>
+                          Завершить
+                          <StopCircleIcon />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {item.h>1 && (
+                    <div className='react-grid-item_more'>
+                      <div className='react-grid-item_top-info'>Операторов на смене:</div>
+                      <div className='react-grid-item_operators'>
+                        {operators.map(({active}:any, i)=>{
+                          return (
+                            <div key={i} className={`react-grid-item_operators-item ${!active && 'disable'}`}>
+                              <div>
+                                <img src={`${process.env.PUBLIC_URL}/ops/${i+1}.jpg`} alt='' />
+                              </div>
+                              <span>
+                                <AccountCircleIcon />
+                              </span>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  )}
+                  {/* <span className='react-grid-item-delete' onClick={()=>onDelete(key)}>
+                    <Close />
+                  </span> */}
                 </div>
               )
         })}
